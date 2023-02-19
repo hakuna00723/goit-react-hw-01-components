489c2f6d21bd708c632260145ce21ef99ca29979
Serhii <110534352+hakuna00723@users.noreply.github.com>
2023-02-19 21:27:39 +0100

    Update App.jsx
    import:
    TransactionHistory.jsx;
    transactions.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,20 @@ import { Statistics } from './Statistics';
 import data from '../data/data.json';
 import { FriendList } from './FriendList';
 import friends from '../data/friends.json';
+import { TransactionHistory } from './TransactionHistory';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
     >
       <Profile
         username={user.username}
@@ -26,6 +28,7 @@ export const App = () => {
       />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
